729715938f33f34cff4be68405a7f46994fcb368
Felipe Pêpe <felipepepe21@gmail.com>
2025-05-07 11:15:00 -0300

    feat: :sparkles: added next-intl

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,13 @@
 import Form from "@/components/form";
 import ThemeToggle from "@/components/themeToggle";
 import { FaBriefcase } from "react-icons/fa";
+import { getTranslations } from "next-intl/server";
 
 export default async function Home() {
+  const t = await getTranslations("HomePage");
   return (
     <div className="my-12 mx-8 flex flex-col justify-center items-center gap-12">
+      <h1>{t("title")}</h1>
       <div className="fixed top-0 left-0">
         <ThemeToggle />
       </div>
